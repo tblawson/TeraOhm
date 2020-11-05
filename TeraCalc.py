c52e6181852fbe268f5f116b335ebd1440ace9e2
Tim Lawson <tim.lawson@callaghaninnovation.govt.nz>
2020-11-06 11:58:18 +1300

    Fixed dict key error - Rs_meas['value'] is now Rs_meas['value(meas)'].

--- TeraCalc.py
+++ TeraCalc.py
@@ -93,11 +93,11 @@ for chan_label in meas_data.keys():  # 'A01', 'A02'...
     """
     Rx_calc = Rx_meas
     Rx_spec = GTC.ureal(0, calc_setup.spec(Rx_meas['value'].x))
-    Rs_spec = GTC.ureal(0, calc_setup.spec(Rs_meas['value'].x))
+    Rs_spec = GTC.ureal(0, calc_setup.spec(Rs_meas['value(meas)'].x))
     u_spec = max(Rx_spec.u, Rs_spec.u)
     spec = GTC.ureal(0, u_spec)
 
-    Rx_calc['value'] = Rs * Rx_meas['value'] / Rs_meas['value'] + spec
+    Rx_calc['value'] = Rs * Rx_meas['value'] / Rs_meas['value(meas)'] + spec
 
     analysed_results.update({chan_label: Rx_calc})
 
